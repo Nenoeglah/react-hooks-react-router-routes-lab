c3270807b096c5c27332aa13a3d3e9fe25d9f380
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-07-27 20:01:48 +0300

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
+
+
+
 import React from "react";
 
 function Home() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* Additional content can be added here */}
+    </div>
+  );
 }
 
 export default Home;
